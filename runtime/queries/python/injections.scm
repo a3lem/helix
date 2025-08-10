ad36abdce1626666b2d9d0a1a8d49389bf78becd
a3lem <me@a3lem.com>
2025-08-10 14:54:15 +0200

    Working # language= injection comments!

--- runtime/queries/python/injections.scm
+++ runtime/queries/python/injections.scm
@@ -1,2 +1,32 @@
 ((comment) @injection.content
  (#set! injection.language "comment"))
+
+(
+  (comment) @injection.language @_comment
+  .
+  [
+    ; Comment above bare string.
+    (expression_statement
+      (string
+        (string_content) @injection.content))
+    ; Comment above string being assigned to variable
+    (expression_statement
+      (assignment
+        right: (string
+          (string_content) @injection.content)))
+    ; comment above string returned by function
+    (block
+      (return_statement
+        (string
+          (string_content) @injection.content)))
+    ; comment above string assigned to class variable
+    (_
+      (expression_statement
+        (assignment
+          right: (string
+            (string_content) @injection.content))))
+
+  ]
+  ( #match? @_comment "^#\\slang(uage)?\\s=[^\\n]+")
+)
+
