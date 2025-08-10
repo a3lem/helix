eb6500264e87358774bdfd53d34389a9552df1d2
a3lem <me@a3lem.com>
2025-08-10 14:54:15 +0200

    Working # language= injection comments!

--- runtime/queries/python/injections.scm
+++ runtime/queries/python/injections.scm
@@ -12,3 +12,32 @@
     . (string
         (string_content) @injection.content))
   (#set! injection.language "regex"))
+
+(
+  (comment) @injection.language @_comment
+  .
+  [
+    ; Comment above bare string.
+    (expression_statement
+      (string
+        (string_content) @injection.content))
+    ; Comment above string being assigned to variable
+    (expression_statement
+      (assignment
+        right: (string
+          (string_content) @injection.content)))
+    ; comment above string returned by function
+    (block
+      (return_statement
+        (string
+          (string_content) @injection.content)))
+    ; comment above string assigned to class variable
+    (_
+      (expression_statement
+        (assignment
+          right: (string
+            (string_content) @injection.content))))
+
+  ]
+  ( #match? @_comment "^#\\slang(uage)?\\s=[^\\n]+")
+)
